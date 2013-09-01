acafa4c2d3a9981db8948a32e5e5846f01c1dbf8
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-09-01 11:23:11 -0700

    Finished writing the insertHourlyTotal
    However, it has not been implemented, yet.

--- app.js
+++ app.js
@@ -4,9 +4,10 @@
 
 var INTERVAL   = 10 * 1000;
 
-var request    = require('request');
-var mysql      = require('mysql');
-var async      = require('async');
+var utils = require('./lib/utils');
+var request = require('request');
+var mysql = require('mysql');
+var async = require('async');
 
 var settings = require('./lib/settings.js');
 
@@ -117,21 +118,59 @@ var insertConsumptionData = function (id, kW, kWh, callback) {
   );
 };
 
-var insertHourlyTotal = function (id, kW, kWh, callback) {
+/**
+ * So there are hourly totals in the `hourly_totals` table. It has a
+ * `start_kwh` and a `hour_kwh` column. The `start_kwh` column represents the
+ * kWh reading at the start of the hour, and `hour_kwh` is the cumulated kWh
+ * within that hour. In other words, `hour_kwh = kWh - start_kwh`.
+ *
+ * @param deviceId represents the foreign key to a record in the `devices`
+ *   table in the database.
+ * @param kWh is the current killowatt hour of a particular device.
+ * @param callback is a function that is called once data has gone into the
+ *   database.
+ */
+var insertHourlyTotal = function (deviceId, kWh, callback) {
   connection.query(
-    'SELECT id, time, start_kwh, hour_kwh, device_id ' +
+    'SELECT device_id, time, start_kwh, hour_kwh ' +
     'FROM hourly_totals ' +
-    'WHERE device_id=' + connection.escape(id) + ' ' +
-    'ORDER BY time DESC ' + 
-    'LIMIT 1',
+    'WHERE device_id=' + connection.escape(deviceId) + ' ' +
+    'ORDER BY time DESC ' +
+    'LIMIT 1;',
 
     function (err, results) {
-      if (!results.length) {
+      var currentTime = new Date();
+      var result = results[0];
+
+      var queryDone = function (err) {
+        if (err) return callback(err);
+        callback(null);
+      };
+
+      if (
+        !results.length ||
+        !utils.areHoursSame(result.time, currentTime)
+      ) {
         return connection.query(
           'INSERT INTO hourly_totals ' +
-          '()'
+          '(device_id, time, start_kwh, hour_kwh) ' +
+          'VALUE ( ' +
+            connection.escape(deviceId) + ', ' +
+            connection.escape( currentTime.getTime() ) + ', ' +
+            connection.escape(kWh) + ', ' +
+            connection.escape(0) + ' ' +
+          ');',
+          queryDone
         );
       }
+
+      connection.query(
+        'UPDATE hourly_totals ' +
+        'SET hour_kwh=' + connection.escape( kWh - result.start_kwh ) + ' ' +
+        'WHERE device_id=' + connection.escape(deviceId) +
+        ';',
+        queryDone
+      );
     }
   );
 };
@@ -144,10 +183,13 @@ setInterval(function () {
       var consumptions = JSON.parse(body);
 
       consumptions.forEach(function (device) {
+
+        // TODO: rewrite this function so that the `result` value, below, is
+        //   not required to be propagated at more than one callback.
         async.waterfall([
 
-          // Get the device's database ID, given its device number. If the
-          // device isn't even in the database, then insert it.
+          // Get the device's database record, given its device number. If the
+          // device isn't in the database, then insert it.
           function (callback) {
 
             getDeviceFromNumber(device.deviceNumber, function (err, device) {
@@ -163,10 +205,11 @@ setInterval(function () {
               result.id, device.kW, device.kWh,
               function (err) {
                 if (err) return callback(err);
-                callback(null);
+                callback(null, result);
               }
             );
           }
+
         ],
         function (err) {
           if (err) return console.error(err);
